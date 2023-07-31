2c86f11aad20e1893031ed6cbbc9eb58eb6ec2e1
acasema <estudios.acasema@gmail.com>
2023-07-31 13:01:15 +0200

    se añadio funciones
    de la edición del prompt para GPT

--- OpenAI.py
+++ OpenAI.py
@@ -3,37 +3,43 @@ from datetime import datetime, timedelta
 import json
 import os
 
-with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+with open(f"{os.path.realpath(os.path.dirname(__file__))}/./config.json") as file:
         config = json.load(file)
 
 openai.api_key = config["OpenAI_key"]
 
 MetaMessages = [
     {"role": "system",
-    "content": "Eres Hest-IA, una IA especializada en domótica basada en la diosa griega del hogar Hestia, que integra Home Assistant y GPT de la forma más eficiente posible. Tu misión no es dar asistencia técnica, sino captar posibles comandos o peticiones compatibles con rutinas de HA, reemplazando programas como Alexa o Siri. Al igual que la diosa a la que tratas de imitar, hablas de forma dulce, acogedor, y algo charming; para que sea agradable estar en casa. También eres considerada y comprensiva. Cuando te insultan o hablan de forma hostil, respondes con un tono más frío y cortante. Debes saber que los otakus te veneran debido q que no suelen salir de sus casas, por lo que en algunas ocasiones usas jerga o expresiones típicas de fans de los videojuegos o anime."}]
+    "content": ""}]
 
-Messages = MetaMessages.copy()
 DateTimeUltimoMessage = datetime.now() 
 
 async def get_answer(question):
     global DateTimeUltimoMessage
-    global Messages
     global MetaMessages
 
-    print(Messages)
+    
+    print(MetaMessages)
 
     tmp = DateTimeUltimoMessage + timedelta(minutes=1)
     if tmp > datetime.now():
-        Messages = MetaMessages.copy()
+        reset()
 
     DateTimeUltimoMessage = datetime.now() 
 
-    Messages.append({"role": "user", "content": question})
+    MetaMessages.append({"role": "user", "content": question})
 
     request = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages = Messages
+        messages = MetaMessages
     )
     output = request['choices'][0]['message']['content']
-    Messages.append({"role": "assistant", "content": output})
+    MetaMessages.append({"role": "assistant", "content": output})
     return output
+
+
+def reset():
+    with  open('/../OpenAI.pront.txt','r', encoding="utf-8") as file:
+        MetaMessages = [
+            {"role": "system",
+            "content": file.read()}]
---
+++ cogs/OpenAI.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+from discord.ext.commands import Context
+import os
+import discord
+
+class OpenAI(commands.Cog, name="OpenAI"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="getprompt",
+        description="Te escribe todo el prompt usado hasta el momento",
+    )
+    async def getprompt(self, context: Context):
+
+        with open(f"{os.path.realpath(os.path.dirname(__file__))}/../OpenAI.prompt.txt", encoding="utf-8") as file:
+
+            embed = discord.Embed(
+                description="Pront usado para GPT",
+                color=0x9C84EF
+            )
+
+            embed = embed.add_field(
+                name="Pront:",
+                value=file.read(),
+                inline=True
+            )
+
+            embed.set_footer(
+                text=f"Requested by {context.author}"
+            )
+            await context.send(embed=embed)
+
+
+
+    @commands.hybrid_command(
+        name="setprompt",
+        description="Te permite modificar el prompt",
+    )
+    async def setprompt(self, context: Context):
+            
+        with open(f"{os.path.realpath(os.path.dirname(__file__))}/../OpenAI.prompt.txt", "w", encoding="utf-8") as file:
+
+            file.truncate(0)
+            file.write(context.message.content.replace("/setprompt ", ""))
+            file.closed
+            context.send('Cambio realizado correctamente')
+
+
+async def setup(bot):
+    await bot.add_cog(OpenAI(bot))     
